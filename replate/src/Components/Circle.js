91b8fa787811cd0e948c30180e09fc84fbcd5a77
theolamide <ajadidan@gmail.com>
2020-11-01 23:31:25 -0500

    separating singlecircle from circle component

--- replate/src/Components/Circle.js
+++ replate/src/Components/Circle.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React , {useState} from 'react';
+import SingleCircle from './SIngleCircle.js';
 
 const Circle = () => {
 
+    const [circleProps, setCircleProps] = useState()
     return(
         <div>
-            <svg width="20rem" height="20rem" style={{border: "1px solid black", marginTop: "5rem"}}>
-                <circle cx="10rem" cy="10rem" r="5rem" stroke="green" strokeWidth="2" fill="yellow" />
-            </svg>
-            <svg width="20rem" height="20rem" style={{border: "1px solid black", marginTop: "5rem"}}>
-                <circle cx="10rem" cy="10rem" r="5rem" stroke="green" strokeWidth="2" fill="yellow" />
-            </svg>
+            <SingleCircle />
         </div>
     )
 }
---
+++ replate/src/Components/SIngleCircle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SingleCircle = () => {
+
+    return(
+        <div>
+            <svg width="20rem" height="20rem" style={{border: "1px solid black", marginTop: "5rem"}}>
+                <circle cx="10rem" cy="10rem" r="5rem" stroke="green" strokeWidth="2" fill="yellow" />
+            </svg>
+
+        </div>        
+    )
+}
+
+export default SingleCircle;
